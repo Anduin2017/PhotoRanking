67393f62518cd9a771031de2a848a6a140af21a2
Anduin Xue <anduin@aiursoft.com>
2026-02-09 13:25:53 +0000

    Edit ScoringService.cs

--- src/Anduin.PhotoRanking/Services/ScoringService.cs
+++ src/Anduin.PhotoRanking/Services/ScoringService.cs
@@ -65,7 +65,7 @@ public class ScoringService
         {
             var isEligibleForSix = photo.RatingCount > 8 &&
                                   Math.Round(photo.IndependentScore ?? 0) >= 5 &&
-                                  photo.Album.AlbumScore > 3.8;
+                                  photo.Album.AlbumScore > 3.1;
             
             if (!isEligibleForSix)
             {
